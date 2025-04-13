e52a0ecb2eb110d883c6df2252cc0b15ed8bc2f9
prs2002 <praneetsuyal+github@gmail.com>
2025-04-14 04:44:40 +0530

    deployment bug fixes

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 //     allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers
 //   }));
 app.use(cors({
-  origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
+  origin: 'https://salesblink-prs.vercel.app/',
   credentials: true,
 }));
 
