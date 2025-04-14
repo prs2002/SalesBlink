7dff9160af54947605295211ffa9b5d0f7111599
prs2002 <praneetsuyal+github@gmail.com>
2025-04-14 15:04:21 +0530

    debugging

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,27 +29,28 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  const token = req.cookies.jwt;
+  // const token = req.cookies.jwt;
+  console.log('🔐 /auth route hit');
 
   if (token) {
-    try{
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded._id).select('-password');
-      res.sendStatus(200);
-    }
-    catch(e){
-      console.error(e);
-      res.status(401).json({ message: 'Not authorized, invalid token' });
-    }
-    // jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    //   if (err) {
-    //     // Token is invalid or expired
-    //     return res.status(401).json({ message: 'Not authorized, invalid token' });
-    //   } else {
-    //     // Token is valid
-    //     return res.sendStatus(200);
-    //   }
-    // });
+  //   try{
+  //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  //     req.user = await User.findById(decoded._id).select('-password');
+  //     res.sendStatus(200);
+  //   }
+  //   catch(e){
+  //     console.error(e);
+  //     res.status(401).json({ message: 'Not authorized, invalid token' });
+    // }
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        // Token is invalid or expired
+        return res.status(401).json({ message: 'Not authorized, invalid token' });
+      } else {
+        // Token is valid
+        return res.sendStatus(200);
+      }
+    });
   } else {
     // No token in the cookie
     return res.status(401).json({ message: 'Not authorized, no token' });
