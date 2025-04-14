6891e08e8e6862c24817e65cb7729f94d9e1b36a
prs2002 <praneetsuyal+github@gmail.com>
2025-04-14 11:52:53 +0530

    deployment bugs

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ const allowCrossDomain = (req, res, next) => {
   res.header(`Access-Control-Allow-Origin`, `https://salesblink-prs.vercel.app`);
   res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+  res.header(`Access-Control-Allow-Credentials`, `true`);
   next();
 };
 
